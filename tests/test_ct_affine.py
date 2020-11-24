a6b0bd4cde3b95001139d53a396a9f4f633e5336
ikuchin <ivan.kuchin@gmail.com>
2020-11-23 21:32:09 -0500

    Den-s CT-s affine unit test added

---
+++ tests/test_ct_affine.py
@@ -0,0 +1,67 @@
+import unittest
+import nibabel as nib
+import glob
+import os
+import pydicom
+import numpy as np
+
+patients_folder = "c:\\docs\\src\\kt\\datasets\\den\\data_by_series\\"
+
+
+class test_label_affine(unittest.TestCase):
+    def get_patient_orientations(self, file_name):
+        slice = pydicom.dcmread(file_name)
+        return slice[0x20, 0x37].value
+
+    def get_patient_position(self, file_name):
+        slice = pydicom.dcmread(file_name)
+        return slice[0x20, 0x32].value
+
+
+    def analyze(self, file_name, affine):
+        if affine[2, 2] != -1:
+            print("-----", file_name, "\n", affine)
+        # sum = affine[1, 0] + affine[0, 1] + affine[2, 0] + affine[0, 2] + affine[2, 1] + affine[1, 2] + affine[3, 0] + affine[0, 3] + affine[3, 1] + affine[1, 3] + affine[3, 2] + affine[2, 3] + affine[3, 3]
+        # if sum != 1:
+        #     print("-----", file_name, "\n", affine)
+
+    def analyze_translation(self, affine):
+        ''' analyze if PCS (patient coordinate system) located out of (0, 0, 0) '''
+        result = True
+        sum = affine[0] + affine[1] + affine[2]
+        if sum == 0:
+            result = False
+
+        return result
+
+    def analyze_rotation(self, affine):
+        ''' analyze if patient oriented along two perpendicular axis '''
+        result = True
+        sum = np.sum(np.abs(np.asarray(affine)))
+        if sum != 2:
+            result = False
+
+        return result
+
+    def test_is_translation_is_0(self):
+        for patient_folder in glob.glob(os.path.join(patients_folder, "*")):
+            for series_folder in glob.glob(os.path.join(patient_folder, "*")):
+                for dcm_file in glob.glob(os.path.join(series_folder, "*.dcm")):
+                    affine = self.get_patient_position(dcm_file)
+                    analysis_result = self.analyze_translation(affine)
+                    self.assertEqual(True, analysis_result, msg = "translation is non-zero in file {}".format(dcm_file))
+                    break
+
+
+    def test_is_rotation_is_0(self):
+        for patient_folder in glob.glob(os.path.join(patients_folder, "*")):
+            for series_folder in glob.glob(os.path.join(patient_folder, "*")):
+                for dcm_file in glob.glob(os.path.join(series_folder, "*.dcm")):
+                    affine = self.get_patient_orientations(dcm_file)
+                    analysis_result = self.analyze_rotation(affine)
+                    self.assertEqual(True, analysis_result, msg = "rotation is non-zero in file {}".format(dcm_file))
+                    break
+
+
+if __name__ == "__main__":
+    unittest.main()
--- tests/test_labels_affine.py
+++ tests/test_labels_affine.py
@@ -5,12 +5,12 @@ import os
 
 nifti_folder = "c:\\docs\\src\\kt\\datasets\\ct-150\\labels\\"
 
+
 class test_label_affine(unittest.TestCase):
     def get_affine_from_nifti_file(self, file_name):
         nifti = nib.load(file_name)
         affine = nifti.affine
         return affine
-
 
     def analyze(self, file_name, affine):
         if affine[2, 2] != -1:
@@ -18,7 +18,6 @@ class test_label_affine(unittest.TestCase):
         # sum = affine[1, 0] + affine[0, 1] + affine[2, 0] + affine[0, 2] + affine[2, 1] + affine[1, 2] + affine[3, 0] + affine[0, 3] + affine[3, 1] + affine[1, 3] + affine[3, 2] + affine[2, 3] + affine[3, 3]
         # if sum != 1:
         #     print("-----", file_name, "\n", affine)
-
 
     def analyze_translation(self, affine):
         result = True
@@ -28,7 +27,6 @@ class test_label_affine(unittest.TestCase):
 
         return result
 
-
     def analyze_rotation(self, affine):
         result = True
         sum = affine[1, 0] + affine[0, 1] + affine[2, 0] + affine[0, 2] + affine[2, 1] + affine[1, 2]
@@ -37,13 +35,14 @@ class test_label_affine(unittest.TestCase):
 
         return result
 
-
-    def test_translation_is_0(self):
+    def test_is_translation_is_0(self):
         for file_name in glob.glob(os.path.join(nifti_folder, "label*.nii")):
             affine = self.get_affine_from_nifti_file(file_name)
             analysis_result = self.analyze_translation(affine)
             self.assertEqual(True, analysis_result, msg = "translation is non-zero in file {}".format(file_name))
 
+    def test_is_rotation_is_0(self):
+        for file_name in glob.glob(os.path.join(nifti_folder, "label*.nii")):
             affine = self.get_affine_from_nifti_file(file_name)
             analysis_result = self.analyze_rotation(affine)
             self.assertEqual(True, analysis_result, msg = "rotation is non-zero in file {}".format(file_name))
@@ -51,5 +50,3 @@ class test_label_affine(unittest.TestCase):
 
 if __name__ == "__main__":
     unittest.main()
-
-
