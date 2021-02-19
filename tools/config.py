d15efb97302236a8914b4e2e14c297bf312a2846
ikuchin <ivan.kuchin@gmail.com>
2021-02-19 00:04:37 -0500

    code deduplication

--- tools/config.py
+++ tools/config.py
@@ -19,4 +19,3 @@ IMAGE_DIMENSION_Z = IMAGE_DIMENSION_X
 MODEL_CHECKPOINT = "weights.hdf5"
 
 TFRECORD_FOLDER = "/docs/src/kt/datasets/ct-150/tfrecords/{}x{}x{}/".format(IMAGE_DIMENSION_X, IMAGE_DIMENSION_Y, IMAGE_DIMENSION_Z)
-TFRECORD_FOLDER = "/docs/src/kt/datasets/ct-150/tfrecords/{}x{}x{}/".format(IMAGE_DIMENSION_X, IMAGE_DIMENSION_Y, IMAGE_DIMENSION_Z)
