e49a45b19438351217502abbed2219f57defb4be
ikuchin <ivan.kuchin@gmail.com>
2021-02-18 23:27:29 -0500

    att-unet built

--- tools/craft_network/att_unet.py
+++ tools/craft_network/att_unet.py
@@ -2,11 +2,12 @@ import tensorflow as tf
 import os
 
 from tools.predict_on_random_data import predict_on_random_data
+from tools.craft_network.att_gate import attention_gate
 
 import tools.config as config
 
 
-def double_conv(filters, kernel_size=[3,3,3], apply_batchnorm=True, apply_dropout=False):
+def double_conv(filters, kernel_size=[3,3,1], apply_batchnorm=True, apply_dropout=False):
     model = tf.keras.models.Sequential()
 
     model.add(tf.keras.layers.Conv3D(filters, kernel_size = kernel_size, padding = "same", kernel_initializer='he_uniform'))
@@ -25,38 +26,42 @@ def double_conv(filters, kernel_size=[3,3,3], apply_batchnorm=True, apply_dropou
 
     return model
 
+def get_gatimg_base(filters, apply_batchnorm = True):
+    __model = tf.keras.models.Sequential()
+
+    __model.add(tf.keras.layers.Conv3D(filters, kernel_size = 1, padding = "same"))
+    if apply_batchnorm:
+      __model.add(tf.keras.layers.BatchNormalization(momentum = config.BATCH_NORM_MOMENTUM))
+
+    return __model
 
 def craft_network(checkpoint_file = None, apply_batchnorm=True):
-    downsample_steps = [
-        double_conv(32, [3,3,1]),   # (?, 128, 128, __filters__)
-        double_conv(64, [3,3,1]),   # (?,  64,  64, __filters__)
-        double_conv(128, [3,3,1]),  # (?,  32,  32, __filters__)
-        double_conv(256, [3,3,1]),  # (?,  16,  16, __filters__)
-    ]
-
-    upsample_steps = [
-        double_conv(128, [3,3,1]),  # (?,  32,  32, __filters__)
-        double_conv(64, [3,3,1]),   # (?,  64,  64, __filters__)
-        double_conv(32, [3,3,1]),   # (?, 128, 128, __filters__)
-    ]
-
-    filters = [32, 64, 128, 256]
+    filters = [16, 32, 64, 128, 256]
 
     inputs = tf.keras.layers.Input(shape = [config.IMAGE_DIMENSION_X, config.IMAGE_DIMENSION_Y, config.IMAGE_DIMENSION_Z, 1])
 
     x = inputs
     generator_steps_output = []
     for idx, _filter in enumerate(filters):
-        x = double_conv(_filter, apply_batchnorm=apply_batchnorm)(x)
+        x = double_conv(_filter, kernel_size = config.KERNEL_SIZE, apply_batchnorm = apply_batchnorm)(x)
         generator_steps_output.append(x)
         if idx < len(filters) - 1:
             x = tf.keras.layers.MaxPool3D(pool_size=(2, 2, 1), padding = "same")(x)
 
+    gating_base = get_gatimg_base(filters[-2], apply_batchnorm)(x)
+
     skip_conns = reversed(generator_steps_output[:-1])
     for _filter, skip_conn in zip(reversed(filters[:-1]), skip_conns):
         x = tf.keras.layers.Conv3DTranspose(_filter, kernel_size = [4,4,1], strides = [2,2,1], padding = "same", kernel_initializer='he_uniform')(x)
-        x = tf.keras.layers.Concatenate(name = "concat_{}".format(_filter))([x, skip_conn])
-        x = double_conv(_filter, apply_batchnorm=apply_batchnorm)(x)
+
+        if _filter == filters[0]:
+            # --- don't gate signal due to no useful features at top level
+            gated_skip = skip_conn
+        else:
+            gated_skip = attention_gate(skip_conn, gating_base, apply_batchnorm = apply_batchnorm)
+
+        x = tf.keras.layers.Concatenate(name = "concat_{}".format(_filter))([x, gated_skip])
+        x = double_conv(_filter, kernel_size = config.KERNEL_SIZE, apply_batchnorm = apply_batchnorm)(x)
 
     output_layer = tf.keras.layers.Conv3D(2, kernel_size = 1, padding = "same", kernel_initializer = "he_uniform")(x)
 
