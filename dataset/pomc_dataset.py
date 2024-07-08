f009de561e0e8e071b8226b3bc13773ba5994304
Ivan Kuchin (ikuchin) <ivan.kuchin@gmail.com>
2024-07-08 19:10:26 -0400

    Bugfix: files _nrrd and _dcm are ignored

--- dataset/pomc_dataset.py
+++ dataset/pomc_dataset.py
@@ -82,7 +82,9 @@ class POMCDataset:
 
     def get_dicom_data(self, folder):
         result = np.array([])
-        file_list = glob.glob(os.path.join(folder, "*.dcm"))
+        # *dcm instead of *.dcm let us to read files like this
+        # 1-1_dcm and 1-2.dcm
+        file_list = glob.glob(os.path.join(folder, "*dcm"))
         if len(file_list):
             result, metadata = self.read_dicom_data_from_files(file_list)
             if result.shape[0]:
@@ -100,7 +102,7 @@ class POMCDataset:
 
     def get_nrrd_data(self, folder):
         result = np.array([])
-        file_list = glob.glob(os.path.join(folder, "*.nrrd"))
+        file_list = glob.glob(os.path.join(folder, "*nrrd"))
         if len(file_list):
             result, metadata = self.read_nrrd_data_from_files(file_list)
             if result.shape[0]:
