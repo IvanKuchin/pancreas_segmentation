cef929e91bbbc41fe1561b8476d7b3f2ce5eb0f8
Ivan Kuchin (ikuchin) <ivan.kuchin@gmail.com>
2024-07-17 14:46:21 -0400

    Enhancement: inroduce config parameter TRAIN_PASSES_PER_VALIDATION

--- train_segmentation.py
+++ train_segmentation.py
@@ -49,7 +49,7 @@ def main():
     ds_train = craft_datasets(os.path.join(config.TFRECORD_FOLDER, "train"))
     ds_valid = craft_datasets(os.path.join(config.TFRECORD_FOLDER, "valid"))
 
-    ds_train = ds_train.prefetch(1).repeat(2)
+    ds_train = ds_train.prefetch(1).repeat(config.TRAIN_PASSES_PER_VALIDATION)
 
     model = craft_network(config.MODEL_CHECKPOINT)
     # predict_on_random_data(model)
