731ac4f719348a8b9dde78fe4c6b37a73a8a1642
Ivan Kuchin (ikuchin) <ivan.kuchin@gmail.com>
2024-08-11 17:26:51 -0400

    Feature: implemented ResBloc instead of conv_double

--- tools/craft_network/att_unet.py
+++ tools/craft_network/att_unet.py
@@ -14,20 +14,57 @@ from tools.craft_network.att_gate import AttGate
 import config as config
 
 
-def double_conv(filters, kernel_size=[3,3,1], apply_batchnorm=True, apply_dropout=False):
+def res_block(filters, input_shape, kernel_size, apply_batchnorm, apply_instancenorm, apply_dropout=False):
+    input_layer = tf.keras.layers.Input(shape = input_shape[1:])
+
+    # primary path
+    x = tf.keras.layers.Conv3D(filters, kernel_size = kernel_size, padding = "same", kernel_initializer='he_uniform')(input_layer)
+    if (apply_batchnorm):
+        x = tf.keras.layers.BatchNormalization(momentum=config.BATCH_NORM_MOMENTUM)(x)
+    x = tf.keras.layers.LeakyReLU()(x)
+    if (apply_dropout):
+        x = tf.keras.layers.Dropout(0.5)(x)
+
+    x = tf.keras.layers.Conv3D(filters, kernel_size = kernel_size, padding = "same", kernel_initializer='he_uniform')(x)
+    if (apply_batchnorm):
+        x = tf.keras.layers.BatchNormalization(momentum=config.BATCH_NORM_MOMENTUM)(x)
+
+    # residual path
+    res_path = input_layer
+    if (input_shape[-1] != filters):
+        res_path = tf.keras.layers.Conv3D(filters, kernel_size = 1, padding = "same", kernel_initializer='he_uniform')(input_layer)
+        if (apply_batchnorm):
+            res_path = tf.keras.layers.BatchNormalization(momentum=config.BATCH_NORM_MOMENTUM)(res_path)
+
+    # add the residual path
+    x = tf.keras.layers.Add()([res_path, x])
+
+    x = tf.keras.layers.LeakyReLU()(x)
+    if (apply_dropout):
+        x = tf.keras.layers.Dropout(0.5)(x)
+    
+    return tf.keras.models.Model(inputs = [input_layer], outputs = x)
+
+def double_conv(filters, input_shape, kernel_size, apply_batchnorm, apply_instancenorm, apply_dropout=False):
     model = tf.keras.models.Sequential()
 
     model.add(tf.keras.layers.Conv3D(filters, kernel_size = kernel_size, padding = "same", kernel_initializer='he_uniform'))
     if (apply_batchnorm):
         model.add(tf.keras.layers.BatchNormalization(momentum=config.BATCH_NORM_MOMENTUM))
-    model.add(tf.keras.layers.ReLU())
+    if (apply_instancenorm):
+        # Instance normalization is not supported in tf.keras.layers.BatchNormalization
+        model.add(tf.keras.layers.LayerNormalization())
+    model.add(tf.keras.layers.LeakyReLU())
     if (apply_dropout):
         model.add(tf.keras.layers.Dropout(0.5))
 
     model.add(tf.keras.layers.Conv3D(filters, kernel_size = kernel_size, padding = "same", kernel_initializer='he_uniform'))
     if (apply_batchnorm):
         model.add(tf.keras.layers.BatchNormalization(momentum=config.BATCH_NORM_MOMENTUM))
-    model.add(tf.keras.layers.ReLU())
+    if (apply_instancenorm):
+        # Instance normalization is not supported in tf.keras.layers.BatchNormalization
+        model.add(tf.keras.layers.LayerNormalization())
+    model.add(tf.keras.layers.LeakyReLU())
     if (apply_dropout):
         model.add(tf.keras.layers.Dropout(0.5))
 
@@ -43,7 +80,7 @@ def get_gating_base(filters, apply_batchnorm = True):
     return __model
 
 
-def craft_network(checkpoint_file = None, apply_batchnorm=True):
+def craft_network(checkpoint_file = None, apply_batchnorm=True, apply_instancenorm=False):
     filters = [16, 32, 64, 128, 256]
 
     inputs = tf.keras.layers.Input(shape = [config.IMAGE_DIMENSION_X, config.IMAGE_DIMENSION_Y, config.IMAGE_DIMENSION_Z, 1])
@@ -51,7 +88,8 @@ def craft_network(checkpoint_file = None, apply_batchnorm=True):
     x = inputs
     generator_steps_output = []
     for idx, _filter in enumerate(filters):
-        x = double_conv(_filter, kernel_size = config.KERNEL_SIZE, apply_batchnorm = apply_batchnorm)(x)
+        resBlock = res_block(_filter, x.shape, kernel_size = config.KERNEL_SIZE, apply_batchnorm = apply_batchnorm, apply_instancenorm=apply_instancenorm)
+        x = resBlock(x)
         generator_steps_output.append(x)
         if idx < len(filters) - 1:
             x = tf.keras.layers.MaxPool3D(pool_size=(2, 2, 1), padding = "same")(x)
@@ -70,7 +108,7 @@ def craft_network(checkpoint_file = None, apply_batchnorm=True):
             gated_skip = AttGate(apply_batchnorm = apply_batchnorm)((skip_conn, gating_base))
 
         x = tf.keras.layers.Concatenate(name = "concat_{}".format(_filter))([x, gated_skip])
-        x = double_conv(_filter, kernel_size = config.KERNEL_SIZE, apply_batchnorm = apply_batchnorm)(x)
+        x = res_block(_filter, x.shape, kernel_size = config.KERNEL_SIZE, apply_batchnorm = apply_batchnorm, apply_instancenorm = apply_instancenorm)(x)
 
     output_layer = tf.keras.layers.Conv3D(2, kernel_size = 1, padding = "same", kernel_initializer = "he_uniform", activation="softmax")(x)
 
@@ -87,7 +125,7 @@ def craft_network(checkpoint_file = None, apply_batchnorm=True):
 
 
 def main():
-    model = craft_network("checkpoints/weights.keras", apply_batchnorm = config.BATCH_NORM)
+    model = craft_network("checkpoints/weights.keras", apply_batchnorm = config.BATCH_NORM, apply_instancenorm = config.BATCH_NORM)
 
     model.summary(line_length = 128, expand_nested = True, show_trainable = True)
     predict_on_random_data(model)
