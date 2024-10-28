c55926ef43bee939aee5618113483b7ed76a4dad
Ivan Kuchin (ikuchin) <ivan.kuchin@gmail.com>
2024-10-28 17:51:52 -0400

    move loss func to the separate file
    rename loss_func_generator

--- train_segmentation.py
+++ train_segmentation.py
@@ -6,6 +6,7 @@ import os
 from dataset.craft_datasets import craft_datasets
 from tools.categorical_metrics import CategoricalMetric, CategoricalF1, CustomCounter, CustomReduceMetric
 from tools.craft_network import craft_network
+from tools.craft_network.loss import loss_func_generator
 import config as config
 
 
@@ -20,65 +21,6 @@ def get_csv_dir():
     run_id = time.strftime("run_%Y_%m_%d_%H_%M_%S")
     return os.path.join(root_log_dir, run_id)
 
-
-def __dice_coef(y_true, y_pred):
-    gamma = 1.0
-    y_true = tf.cast(y_true, dtype = tf.float32)
-    y_pred = tf.cast(y_pred[..., 1:2], dtype = tf.float32)
-
-    # print("y_true shape: ", y_true.shape)
-    # print("y_pred shape: ", y_pred.shape)
-
-    intersection = tf.reduce_sum(y_true * y_pred)
-    union = tf.reduce_sum(y_true) + tf.reduce_sum(y_pred)
-    dice = (2. * intersection + gamma) / (union + gamma)
-    return dice
-
-def __dice_loss(y_true, y_pred):
-    return 1 - __dice_coef(y_true, y_pred)
-
-
-def __weighted_loss(y_true, y_pred):
-    scaler = 1
-
-    y_true = tf.cast(y_true, dtype = tf.float32)
-    y_pred = tf.cast(y_pred, dtype = tf.float32)
-
-    # count_0 = tf.reduce_sum(tf.cast(y_true == 0.0, y_true.dtype))
-    # count_1 = tf.reduce_sum(tf.cast(y_true == 1.0, y_true.dtype))
-    # background_weight = (1 - count_0 / (count_0 + count_1)) * config.LOSS_SCALER
-    # foreground_weight = (1 - count_1 / (count_0 + count_1)) * config.LOSS_SCALER / 5
-
-    # background_weight = config.BACKGROUND_WEIGHT  # incorrect loss calculations
-    # foreground_weight = config.FOREGROUND_WEIGHT  # incorrect loss calculations
-    # foreground_weight -= background_weight  # incorrect loss calculations
-
-    # loss function described in the paper https://arxiv.org/pdf/1803.05431v2
-    foreground_size = tf.reduce_sum(tf.cast(y_true == 1.0, y_true.dtype))
-    background_size = tf.reduce_sum(tf.cast(y_true == 0.0, y_true.dtype))
-    size = tf.cast(tf.size(y_true), dtype = tf.float32)
-    foreground_weight = (1.0 - foreground_size / size) * scaler
-    background_weight = (1.0 - background_size / size)
-    # foreground and background weights must add up to 1 
-    # it will be added/reversed later
-    foreground_weight -= background_weight
-
-    scce = tf.keras.losses.SparseCategoricalCrossentropy(from_logits = False)
-    loss = scce(
-        tf.maximum(y_true, 0.0),  # remove -1 values from mask,
-        y_pred,
-        sample_weight = tf.maximum(y_true * foreground_weight + background_weight, 0.0),
-    )
-
-    return loss
-
-def loss_generator(loss_name):
-    if loss_name == "dice":
-        return __dice_loss
-    elif loss_name == "scce":
-        return __weighted_loss
-    else:
-        raise ValueError("Unknown loss function")
 
 def main():
     ds_train = craft_datasets(os.path.join(config.TFRECORD_FOLDER, "train"))
@@ -111,7 +53,7 @@ def main():
                                                         learning_rate = config.INITIAL_LEARNING_RATE,
                                                         # gradient_accumulation_steps = config.GRADIENT_ACCUMULATION_STEPS,
                                                         ),
-                  loss = loss_generator(config.LOSS_FUNCTION),
+                  loss = loss_func_generator(config.LOSS_FUNCTION),
                   metrics = [
                       'accuracy',
                       CategoricalMetric(tf.keras.metrics.TruePositives(), name = 'custom_tp'),
