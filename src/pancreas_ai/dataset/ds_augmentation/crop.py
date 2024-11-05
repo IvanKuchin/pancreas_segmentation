98c0fcb2728fe8a5930c81d4bcfa070f75fb96e3
Ivan Kuchin (ikuchin) <ivan.kuchin@gmail.com>
2024-11-04 20:00:47 -0500

    bugfix: sample from shorter range

--- src/pancreas_ai/dataset/ds_augmentation/crop.py
+++ src/pancreas_ai/dataset/ds_augmentation/crop.py
@@ -19,10 +19,13 @@ def random_crop_data_and_label(data, label, x, y, z) -> tuple[np.ndarray, np.nda
     _label = __crop(label, random_offset, [x, y, z])
     return _data, _label
 
-def random_crop_data(data, x, y, z) -> np.ndarray:
+def random_crop_data(data, inside_width, inside_height, inside_depth) -> np.ndarray:
     data_shape = np.shape(data)
-    random_range = [data_shape[0] - x + 1, data_shape[1] - y + 1, data_shape[2] - z + 1]
+    random_range = [
+        (data_shape[0] - inside_width + 1) / 2, 
+        (data_shape[1] - inside_height + 1) / 2, 
+        (data_shape[2] - inside_depth + 1) / 2]
     random_offset = np.random.randint(0, random_range, size = 3)
 
-    return __crop(data, random_offset, [x, y, z])
+    return __crop(data, random_offset, [inside_width, inside_height, inside_depth])
 
